6ac65fa419e9e544f72efc46b71f78b025e9f398
Diego Nascimento <diego.nascimento@liferay.com>
2020-09-23 20:58:19 -0300

    Adds a Input and Select components

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import ClayAlert from "@clayui/alert";
+import ClayForm, {ClaySelectWithOption} from "@clayui/form";
+import {ClayInput} from "@clayui/form";
 import * as React from "react";
 import ReactDOM from "react-dom";
 import "@clayui/css/lib/css/atlas.css";
@@ -6,7 +8,44 @@ import "@clayui/css/lib/css/atlas.css";
 function App() {
   return (
     <div className="App">
-      <h1>{"Hello, my empty React application"}</h1>
+      <ClayForm>
+        <div className="sheet">
+          <ClayAlert displayType="info" title="Hello">
+            {"Let's add some Clay components to see the magic happen!"}
+          </ClayAlert>
+
+          <ClayForm.Group>
+            <label>{"Name"}</label>
+            <ClayInput
+              aria-label="Input for name"
+              placeholder="Insert your name"
+            />
+          </ClayForm.Group>
+
+          <ClayForm.Group>
+            <label>{"Gender"}</label>
+            <ClaySelectWithOption
+              aria-label="Select for gender"
+              defaultValue="other"
+              options={[
+                {
+                  label: "Male",
+                  value: "male"
+                },
+                {
+                  label: "Female",
+                  value: "female"
+                },
+                {
+                  label: "Other",
+                  value: "other"
+                }
+              ]}
+              placeholder="Insert your age"
+            />
+          </ClayForm.Group>
+        </div>
+      </ClayForm>
     </div>
   );
 }
