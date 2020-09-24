f8bcce653a063be772819ff212bf77f9ef778da1
Diego Nascimento <diego.nascimento@liferay.com>
2020-09-23 21:25:08 -0300

    Passes spritemap

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import {ClayInput} from "@clayui/form";
 import * as React from "react";
 import ReactDOM from "react-dom";
 import "@clayui/css/lib/css/atlas.css";
+import { ClayIconSpriteContext } from "@clayui/icon";
 
 function App() {
   // State for controlling the rating slider
@@ -21,70 +22,72 @@ function App() {
   ]);
 
   return (
-    <div className="App">
-      <ClayForm>
-        <div className="sheet">
-          <ClayAlert displayType="info" title="Hello">
-            {"Let's add some Clay components to see the magic happen!"}
-          </ClayAlert>
+    <ClayIconSpriteContext.Provider value="icons.svg">
+      <div className="App">
+        <ClayForm>
+          <div className="sheet">
+            <ClayAlert displayType="info" title="Hello">
+              {"Let's add some Clay components to see the magic happen!"}
+            </ClayAlert>
 
-          <ClayForm.Group>
-            <label>{"Name"}</label>
-            <ClayInput
-              aria-label="Input for name"
-              placeholder="Insert your name"
-            />
-          </ClayForm.Group>
+            <ClayForm.Group>
+              <label>{"Name"}</label>
+              <ClayInput
+                aria-label="Input for name"
+                placeholder="Insert your name"
+              />
+            </ClayForm.Group>
 
-          <ClayForm.Group>
-            <label>{"Gender"}</label>
-            <ClaySelectWithOption
-              aria-label="Select for gender"
-              defaultValue="other"
-              options={[
-                {
-                  label: "Male",
-                  value: "male"
-                },
-                {
-                  label: "Female",
-                  value: "female"
-                },
-                {
-                  label: "Other",
-                  value: "other"
-                }
-              ]}
-              placeholder="Insert your age"
-            />
-          </ClayForm.Group>
+            <ClayForm.Group>
+              <label>{"Gender"}</label>
+              <ClaySelectWithOption
+                aria-label="Select for gender"
+                defaultValue="other"
+                options={[
+                  {
+                    label: "Male",
+                    value: "male"
+                  },
+                  {
+                    label: "Female",
+                    value: "female"
+                  },
+                  {
+                    label: "Other",
+                    value: "other"
+                  }
+                ]}
+                placeholder="Insert your age"
+              />
+            </ClayForm.Group>
 
-          <ClayForm.Group>
-            <label>{"From 0 to 100, which is your satisfaction level?"}</label>
-            <ClaySlider onValueChange={setSliderValue} value={sliderValue} />
-          </ClayForm.Group>
+            <ClayForm.Group>
+              <label>{"From 0 to 100, which is your satisfaction level?"}</label>
+              <ClaySlider onValueChange={setSliderValue} value={sliderValue} />
+            </ClayForm.Group>
 
-          <ClayForm.Group>
-            <ClayToggle label="Do you want to receive special discounts?" required />
-          </ClayForm.Group>
+            <ClayForm.Group>
+              <ClayToggle label="Do you want to receive special discounts?" required />
+            </ClayForm.Group>
 
-          <ClayForm.Group>
-            <ClayToggle label="Do you want to receive free gifts?" required />
-          </ClayForm.Group>
+            <ClayForm.Group>
+              <ClayToggle label="Do you want to receive free gifts?" required />
+            </ClayForm.Group>
 
-          <ClayForm.Group>
-            <label>{"Which is your favorite sports team"}</label>
-            <ClayMultiSelect
-              inputName="teams"
-              inputValue={valueTeams}
-              items={teams}
-              onChange={setValueTeams}
-              onItemsChange={setTeams}
-            />
-          </ClayForm.Group>
-        </div>
-      </ClayForm>
-    </div>
+            <ClayForm.Group>
+              <label>{"Which is your favorite sports team"}</label>
+              <ClayMultiSelect
+                inputName="teams"
+                inputValue={valueTeams}
+                items={teams}
+                onChange={setValueTeams}
+                onItemsChange={setTeams}
+              />
+            </ClayForm.Group>
+          </div>
+        </ClayForm>
+      </div>
+    </ClayIconSpriteContext.Provider>
   );
 }
 
